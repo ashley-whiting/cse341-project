2715c32b9a0fcf53f19265ad4e6ce934dacf32cd
Ashley Whiting <whi19052@byui.edu>
2021-11-04 23:44:57 -0500

    prove 08

---
+++ routes/prove08.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+var jsonEngine = require('../controller/pr08.js');
+
+router.get('/', jsonEngine.processJson).post('/', jsonEngine.getIndex);
+module.exports = router;
--- routes/ta03.js
+++ routes/ta03.js
@@ -1,41 +1,9 @@
-// //TA03 PLACEHOLDER
-// const express = require('express');
-// const router = express.Router();
+//TA03 PLACEHOLDER
+const express = require('express');
+const router = express.Router();
+//const ta03Controller = require('../controller/ta03.js');
 
-// const fs  = require('fs');
+// router.get('/', ta03Controller.getProducts);
+// router.get('/search', ta03Controller.getSearchProducts);
 
-// const https = require('https');
-// let url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
-
-// https.get(url, (res) => {
-//   let body = "";
-
-//   res.on("data", (chunk) => {
-//     body += chunk;
-//   });
-
-//  res.on("end", () => {
-//    try {let json = JSON.parse(body);
-//     res.render('pages/ta03', {
-
-//     });
-//    console.log(body);
-
-//  } catch (error) {
-//    console.error(error.message);
-//  };
-// });
-// }).on("error", (error) => {
-//   console.error(error.message);
-// });
-
-// router.get('/', (req, res, next) => {
-//   res.render('pages/ta03', {
-//     title: 'Team Activity 03',
-//     path: '/ta03', // For pug, EJS
-//     activeTA03: true, // For HBS
-//     contentCSS: true, // For HBS
-//   });
-// });
-
-// module.exports = router;
+module.exports = router;
