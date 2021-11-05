aa4221838b8688dfb26886996765a8eeb552fa2e
Ashley Whiting <whi19052@byui.edu>
2021-11-04 22:23:16 -0500

    i don't know

--- index.js
+++ index.js
@@ -10,12 +10,11 @@
  * Udemy course.
  * IMPORTANT: Make sure to run "npm install" in your root before "npm start"
  *******************************************************************************/
-// Our initial setup (package requires, port number setup)
+/// Our initial setup (package requires, port number setup)
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
-
 
 const app = express();
 
@@ -24,7 +23,6 @@ const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
-const ta05Routes = require('./routes/ta05');
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -35,12 +33,11 @@ app
   // For view engine as hbs (Handlebars)
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+  //.use(bodyParser({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
-  .use('/ta05', ta05Routes)
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
